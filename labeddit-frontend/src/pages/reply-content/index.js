38a0ed6e124bdc3d37f26452142b92575f01184d
Medusa-85 <lima.mariana012@gmail.com>
2023-04-12 12:18:08 -0300

    'Post' na replyPage

--- labeddit-frontend/src/pages/reply-content/index.js
+++ labeddit-frontend/src/pages/reply-content/index.js
@@ -4,16 +4,17 @@ import { PostContainerStyled, PostCardStyled } from "../posts/styled"
 import { useEffect, useState } from "react"
 import {TbArrowBigUp, TbArrowBigDown} from "react-icons/tb"
 import {FaRegCommentAlt} from "react-icons/fa"
-import { getPostById, getPosts, getReplies, LikePost, ReplyPost } from "../../constants"
+import { BASE_URL, getPostById, getPosts, getReplies, LikePost, ReplyPost } from "../../constants"
 import { ContentInput } from "../../components/inputs/content"
 import { Button, FormControl, FormHelperText, Stack } from "@chakra-ui/react"
 import { useNavigate } from "react-router-dom"
 import { useForm } from "../../hooks/use-form"
+import axios from "axios"
 
 export const ReplyPage = () => {
     const navigate = useNavigate()
     const {id} = useParams()
-    const [postById, setPostById] = useState()
+    const [postById, setPostById] = useState([])
     const [replies, setReplies] = useState([])
     const [form, onChangeInputs] = useForm({
         reply: "",
@@ -47,7 +48,57 @@ export const ReplyPage = () => {
         .catch((e)=>{
             console.log(e)
         });
-    }, [])     
+    }, [getPostById])     
+
+    const onClickLike = async (id) => {
+        try{
+            const body = {
+                like: true
+            }
+            await axios.put(`${BASE_URL}/reply/${id}/like`, 
+            body,
+            {
+                headers: {
+                    Authorization: localStorage.getItem("labeddit.token")
+                } 
+            })
+            getReplies()
+            .then(data => {
+                setReplies(data)
+            })
+            .catch((e)=>{
+                console.log(e)
+            });
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    const onClickDislike = async (id) => {
+        try{
+            const body = {
+                like: false
+            }
+            await axios.put(`${BASE_URL}/reply/${id}/like`, 
+            body,
+            {
+                headers: {
+                    Authorization: localStorage.getItem("labeddit.token")
+                } 
+            })
+            getReplies()
+            .then(data => {
+                setReplies(data)
+            })
+            .catch((e)=>{
+                console.log(e)
+            });
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+   console.log(`postById ${postById[0]}`)
 
     return (
         <PageContainer>
@@ -55,7 +106,12 @@ export const ReplyPage = () => {
             <FormContainer>
                 <form onSubmit={onSubmit}>
                     <FormControl>
-                        <PostCardStyled>{postById[0].content}</PostCardStyled>
+                        {postById.map((post) => (
+                            <PostCardStyled>
+                                <h5>{post.creator.name}</h5>
+                                <h3>{post.content}</h3>
+                            </PostCardStyled>
+                        ))}
                         <ContentInput
                             value={form.reply}
                             onChange={onChangeInputs}
@@ -65,8 +121,38 @@ export const ReplyPage = () => {
                         type="submit"
                         variant="formMain"
                     >
-                        Cadastrar
+                        Responder
                     </Button>
+                    {replies.map((reply, i) => (
+                            <PostCardStyled key={i}>
+                                <h5>{reply.creator.name}</h5>
+                                <h3>{reply.content}</h3>
+                                <Stack direction='row' spacing={0}>
+                                    <Button 
+                                    leftIcon={<TbArrowBigUp />}
+                                    onClick={()=>{onClickLike(reply.id)}}
+                                    colorScheme='teal' 
+                                    variant='contenReaction'>
+                                        {reply.likes}
+                                    </Button>
+                                    <Button 
+                                    type="text"
+                                    leftIcon={<TbArrowBigDown />} 
+                                    onClick={()=>{onClickDislike(reply.id)}}
+                                    colorScheme='teal' 
+                                    variant='contenReaction'>
+                                        {reply.dislikes}
+                                    </Button>
+                                    <Button 
+                                        rightIcon={<FaRegCommentAlt />} 
+                                        colorScheme='teal' 
+                                        variant='contenReaction'
+                                        // onClick={()=>{goToReplyPage(navigate, post.id)}}
+                                    >
+                                    </Button>       
+                                </Stack>
+                            </PostCardStyled>)
+                      )}
                 </form>
             </FormContainer>
         </PageContainer>
